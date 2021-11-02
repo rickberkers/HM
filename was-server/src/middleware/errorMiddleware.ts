4efcca0c205a598464d4b4a06421d04add19ebe5
rickberkers <rick.berkers@gmail.com>
2021-11-02 01:07:30 +0100

    basic fastify server

--- was-server/src/middleware/errorMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-import {HttpError} from "http-errors";
-
-//TODO differentiate between operational and http errors
-export default function errorMiddleware(error: HttpError, request: Request, response: Response, next: NextFunction) {
-    response.status(error.status).send({
-        message: error.message
-    });
-}
