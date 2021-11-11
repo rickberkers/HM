81868700573f72181f59e8cc2321345909afeea1
rickberkers <rick.berkers@gmail.com>
2021-11-11 02:46:49 +0100

    improve code

--- was-api/src/services/DayService.ts
+++ was-api/src/services/DayService.ts
@@ -1,7 +1,7 @@
 import IDay from "../models/IDay";
 import { Connection } from "typeorm";
 import { IDayService } from "./IDayService";
-import { addDayToDate, createDateRange } from "../utils";
+import { addDaysToDate } from "../utils";
 import { Between } from "typeorm";
 import { Commitment } from "../entities/Commitment";
 import { DayInfo } from "../entities/DayInfo";
@@ -17,12 +17,20 @@ export default class DayService implements IDayService {
        this.commitmentRepo = this.connection.getRepository(Commitment);
     }
 
+    /**
+     * 
+     * @param minDate 
+     * @param householdId 
+     * @param limit 
+     * @returns 
+     */
     public async getDaysByDateAndHouseholdId(
         minDate: Date, 
         householdId: string, 
         limit: number = 20, 
-        maxDate = addDayToDate(new Date(minDate), limit)
     ): Promise<IDay[]> {
+
+        const maxDate = addDaysToDate(new Date(minDate), limit);
 
         const whereClause = { 
             where: {
@@ -31,12 +39,22 @@ export default class DayService implements IDayService {
             }
         };
 
-        // Retrieve data from database
-        const dayInfos = await this.dayInfoRepo.find(whereClause);
-        const commitments = await this.commitmentRepo.find({
+        const dayInfosFind = this.dayInfoRepo.find(whereClause);
+        const commitmentFind = this.commitmentRepo.find({
             order: { day: 'ASC' }, ...whereClause, 
         });
 
+        const queryResult = await Promise.all([commitmentFind, dayInfosFind]);
+        return this.combineCommitmentsAndDayInfos(...queryResult);
+    }
+    
+    /**
+     * Combines commitments and dayInfos to an array of Days
+     * @param commitments 
+     * @param dayInfos 
+     */
+    private combineCommitmentsAndDayInfos(commitments: Commitment[], dayInfos: DayInfo[]) {
+        
         // Group commitments by day for easier access with combining
         const commitmentMap = commitments.reduce((storage: ICommitmentMap, item) => {
             const group = item.day;
@@ -56,6 +74,5 @@ export default class DayService implements IDayService {
         }, []);
 
         return days;
-    }
-    
+    } 
 }
--- was-api/src/utils.ts
+++ was-api/src/utils.ts
@@ -1,43 +1,3 @@
-// /**
-//  * Function to group an array by a specific element
-//  */
-// export const groupBey = <T>(data: T[], key: string) => { // `data` is an array of objects, `key` is the key (or property accessor) to group by
-//     // reduce runs this anonymous function on each element of `data` (the `item` parameter,
-//     // returning the `storage` parameter at the end
-//     return data.reduce((storage, item: T) => {
-//       // get the first instance of the key by which we're grouping
-//       const group = item[key];
-      
-//       // set `storage` for this instance of group to the outer scope (if not empty) or initialize it
-//       storage[group] = storage[group] || [];
-      
-//       // add this item to its group within `storage`
-//       storage[group].push(item);
-      
-//       // return the updated storage to the reduce function, which will then loop through the next 
-//       return storage; 
-//     }, {}); // {} is the initial value of the storage
-// };
-
-
-// export const groupBy = <T>(array: T[], predicate: <T2>(v: T) => T2) => {
-//     return array.reduce((acc, value) => {
-//         const group = predicate(value);
-//         (acc[predicate(value)] ||= []).push(value);
-//         return acc;
-//     }, {} as { [key: T2]: T[] });
-// }
-
-// const data = [
-//     { comment: "abc", forItem: 1, inModule: 1 },
-//     { comment: "pqr", forItem: 1, inModule: 1 },
-//     { comment: "klm", forItem: 1, inModule: 2 },
-//     { comment: "xyz", forItem: 1, inModule: 2 },
-//   ];
-
-// groupBy(data, <T>(v) => v.inModule);
-
-
 /**
  * Creates an array of dates within specified range
  * @param startDate 
@@ -60,8 +20,8 @@
  * @param date 
  * @param amount 
  */
-export const addDayToDate = (date: Date, amount: number) => {
+export const addDaysToDate = (date: Date, amountDays: number) => {
     // Use UTC date to prevent problems with time zones and DST
-    date.setUTCDate(date.getUTCDate() + amount);
+    date.setUTCDate(date.getUTCDate() + amountDays);
     return date;
 };
