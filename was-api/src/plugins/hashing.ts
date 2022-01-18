946379b8e1144a2fe988bb3e447d6d860c17d8b5
rickberkers <rick.berkers@gmail.com>
2022-01-18 20:32:50 +0100

    implemented argon2 hashing as plugin

--- was-api/src/plugins/hashing.ts
+++ was-api/src/plugins/hashing.ts
@@ -5,15 +5,19 @@ import * as argon2 from "argon2";
  * This plugins adds hashing functions using argon2
  */
 export default fp(async (fastify, opts) => {
+
+  const hashingOptions = {
+    memoryCost: fastify.config.HASH_MEMCOST,
+    timeCost: fastify.config.HASH_TIMECOST
+  }
+
   fastify.decorate<HashingFunctions>('hasher', {
-
-    const pepper = fastify.config.
-
     hash: async (data: string) => {
-      return await argon2.hash(data + pepper);
+      return await argon2.hash(data, hashingOptions);
     },
+    //TODO check if lack of options in compare functions gives bad comparison
     compare: async (originalHash: string, data: string) => {
-      return argon2.verify(originalHash, data + pepper);
+      return argon2.verify(originalHash, data, hashingOptions);
     }
   });
 });
