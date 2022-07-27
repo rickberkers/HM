830b8dcbaa322154431188c745e2aa2a5071de7d
Rick <rick.berkers@gmail.com>
2022-07-27 16:24:14 +0200

    integrated ajv-errors

--- was-api/src/app.ts
+++ was-api/src/app.ts
@@ -1,15 +1,30 @@
 import { join } from 'path';
 import AutoLoad from '@fastify/autoload';
-import { FastifyPluginAsync } from 'fastify';
-import env from './plugins/env';
-import typeorm from './plugins/typeorm';
-import services from './plugins/services';
-import sensible from './plugins/sensible';
-import jwt from './plugins/jwt';
-import hashing from './plugins/hashing';
-import auth from './plugins/auth';
-import cookie from './plugins/cookie';
-import cors from './plugins/cors';
+import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
+import env from '@plugins/env';
+import typeorm from '@plugins/typeorm';
+import services from '@plugins/services';
+import sensible from '@plugins/sensible';
+import jwt from '@plugins/jwt';
+import hashing from '@plugins/hashing';
+import auth from '@plugins/auth';
+import cookie from '@plugins/cookie';
+import cors from '@plugins/cors';
+import ajvErrors from 'ajv-errors';
+
+/**
+ * Custom options for instantiating the fastify-cli `--options`
+ * // TODO see how to do this when fastify-cli isn't used, e.g. in production
+ * @see https://github.com/fastify/fastify-cli
+ */
+export const options: FastifyServerOptions = {
+  ajv: {
+    customOptions: {
+      allErrors: true,
+    },
+    plugins: [ajvErrors]
+  }
+}
 
 const app: FastifyPluginAsync = async (fastify): Promise<void> => {
   
@@ -27,7 +42,8 @@ const app: FastifyPluginAsync = async (fastify): Promise<void> => {
   // This loads all plugins defined in routes
   fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
-    ignorePattern: /.*schemas(\.ts|\.js|\.cjs|\.mjs)$/
+    ignorePattern: /.*schemas(\.ts|\.js|\.cjs|\.mjs)$/,
+    routeParams: true
     // Above line ignores schema files
   });
 };
@@ -35,7 +51,7 @@ const app: FastifyPluginAsync = async (fastify): Promise<void> => {
 export default app;
 export { app }
 
-//TODO revise hashing options
+// TODO revise hashing options
 // TODO add helmet and rate-limiter, possibly implement more security measures on production
 // TODO setup tests complex pieces of code
 // TODO Logging sort out
